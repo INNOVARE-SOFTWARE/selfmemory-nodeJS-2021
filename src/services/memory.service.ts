7c87b82fa8bac21589e229cded761e0c1cd11daa
marceloagustini <agustinimarcelo@gmail.com>
2021-02-15 09:58:44 -0300

    email send

--- src/services/memory.service.ts
+++ src/services/memory.service.ts
@@ -1,11 +1,19 @@
 import { /* inject, */ BindingScope, injectable} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {Memory} from '../models';
-import {ChapterRepository, MemoryRepository} from '../repositories';
+import {ChapterRepository, ConfigRepository, MemoryRepository} from '../repositories';
+
+
+let MAILGUN_API_KEY = 'key-6c26133f71600c088ac81b102f6233ec'
+let MAILGUN_DOMAIN = 'sandbox54f649cf47f543d599bd458567d9cb18.mailgun.org'
+let MAILGUN_EMAIL_FROM = 'admin@innovare.com.ar'
+
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class MemoryService {
-  constructor(@repository(MemoryRepository) public memoryRepository: MemoryRepository,
+  constructor(
+    @repository(ConfigRepository) public configRepository: ConfigRepository,
+    @repository(MemoryRepository) public memoryRepository: MemoryRepository,
     @repository(ChapterRepository) public chapterRepository: ChapterRepository) { }
 
 
@@ -58,9 +66,38 @@ export class MemoryService {
           book += '-----------------------'
         })
       }
-      return book
-    }
-    return ''
 
+      const config = await this.configRepository.findOne({
+        where: {
+          userId: userId
+        }
+      })
+      if (config) {
+        this.sendEmail(config.email1, book)
+        if (config.email2)
+          this.sendEmail(config.email2, book)
+      }
+      return {'result': 'sending...'}
+    }
+    return {'result': 'not sending'}
+
+  }
+
+  sendEmail(to: string, txt: string) {
+    var api_key = MAILGUN_API_KEY;
+    var domain = MAILGUN_DOMAIN;
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+    var data = {
+      from: MAILGUN_EMAIL_FROM,
+      to: to,
+      subject: 'Lee las memorias de ' + to,
+      text: txt
+    };
+
+    mailgun.messages().send(data, function (error: any, body: any) {
+      console.log(body)
+      return body
+    });
   }
 }
